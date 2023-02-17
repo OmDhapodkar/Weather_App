05dc7cb420b40e49e88f31dc52063e856f62cc98
OmDhapodkar <om.dhapodkar2020@vitstudent.ac.in>
2023-02-17 11:59:50 +0530

    Handles

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import HistoryWeather from "./components/history/history";
 import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
 import "./App.css";
 import axios from "axios";
-import CF from "./components/CF/cf.js"
+import CF from "./components/CF/cf.js";
 import { useSnackbar } from "notistack";
-import Map from "./components/map/map.js"
+import Map from "./components/map/map.js";
 import Loader from "./components/Loader/Loader.js";
 
 function App() {
@@ -16,18 +16,17 @@ function App() {
   const [currentWeather, setCurrentWeather] = useState(null);
   const [historyWeather, setHistoryWeather] = useState(null);
   const [forecast, setForecast] = useState(null);
-  const [cf,setCF] = useState(true);
-  const [loc,setLoc] = useState({lon:null,lat:null});
-  const [preLoader, setPreLoader] = useState(false);
-
+  const [cf, setCF] = useState(true);
+  const [loc, setLoc] = useState({ lon: null, lat: null });
+  const [preLoader, setPreLoader] = useState(true);
 
   useEffect(() => {
     async function init() {
-      const data = await localStorage.getItem('cf'); 
+      const data = await localStorage.getItem("cf");
       setCF(JSON.parse(data));
     }
     init();
-}, [])
+  }, []);
 
   useEffect(() => {
     setTimeout(() => {
@@ -35,34 +34,32 @@ function App() {
     }, 3000);
   }, [preLoader]);
 
-  useEffect(()=>{
-    if(loc.lat !== null && loc.lon !== null){
-      const currentWeatherFetch = `${WEATHER_API_URL}/current.json?q=${loc.lat},${loc.lon}&key=${WEATHER_API_KEY}`
-      const forecastFetch = `${WEATHER_API_URL}/forecast.json?q=${loc.lat},${loc.lon}&key=${WEATHER_API_KEY}&days=7`
+  useEffect(() => {
+    if (loc.lat !== null && loc.lon !== null) {
+      const currentWeatherFetch = `${WEATHER_API_URL}/current.json?q=${loc.lat},${loc.lon}&key=${WEATHER_API_KEY}`;
+      const forecastFetch = `${WEATHER_API_URL}/forecast.json?q=${loc.lat},${loc.lon}&key=${WEATHER_API_KEY}&days=7`;
       axios
-      .get(currentWeatherFetch)
-      .then((response) => {
-        showSuccessSnack("Succesfull")
-        const weatherResponse = response.data;
-        setCurrentWeather({ city: "Delhi", ...weatherResponse });
-      })
-      .catch((err) => {
-        showErrorSnack("API is Unavailable")
-      });
+        .get(currentWeatherFetch)
+        .then((response) => {
+          showSuccessSnack("Succesfull");
+          const weatherResponse = response.data;
+          setCurrentWeather({ city: "Delhi", ...weatherResponse });
+        })
+        .catch((err) => {
+          showErrorSnack("API is Unavailable");
+        });
       axios
-      .get(forecastFetch)
-      .then((response) => {
-        showSuccessSnack("Succesfull")
-        const forcastResponse = response.data.forecast.forecastday;
-        setForecast({ city: "Delhi", ...forcastResponse });
-      })
-      .catch((err) => {
-        showErrorSnack("API is Unavailable")
-      });
+        .get(forecastFetch)
+        .then((response) => {
+          showSuccessSnack("Succesfull");
+          const forcastResponse = response.data.forecast.forecastday;
+          setForecast({ city: "Delhi", ...forcastResponse });
+        })
+        .catch((err) => {
+          showErrorSnack("API is Unavailable");
+        });
     }
- 
-  },[loc])
-
+  }, [loc]);
 
   const showErrorSnack = (message) => {
     enqueueSnackbar(message, {
@@ -89,41 +86,38 @@ function App() {
 
   const handleOnLocationChange = (searchData) => {
     if (navigator.geolocation) {
-      showSuccessSnack("Succesfull")
+      showSuccessSnack("Succesfull");
       navigator.geolocation.getCurrentPosition(showPosition);
     } else {
       showErrorSnack("Geolocation is not supported by this browser.");
     }
     function showPosition(position) {
-      setLoc(()=>{
+      setLoc(() => {
         return {
-          lon:position.coords.longitude,
-          lat:position.coords.latitude
-        }
-      })
+          lon: position.coords.longitude,
+          lat: position.coords.latitude,
+        };
+      });
     }
-  }
-
+  };
 
   const handleOnSearchChange = (searchData) => {
-
     const [lat, lon] = searchData.value.split(" ");
 
-
-    const currentWeatherFetch = `${WEATHER_API_URL}/current.json?q=${lat},${lon}&key=${WEATHER_API_KEY}`
-    const forecastFetch = `${WEATHER_API_URL}/forecast.json?q=${lat},${lon}&key=${WEATHER_API_KEY}&days=7`
+    const currentWeatherFetch = `${WEATHER_API_URL}/current.json?q=${lat},${lon}&key=${WEATHER_API_KEY}`;
+    const forecastFetch = `${WEATHER_API_URL}/forecast.json?q=${lat},${lon}&key=${WEATHER_API_KEY}&days=7`;
     // const historyFetch = `${WEATHER_API_URL}/history.json?q=${lat},${lon}&key=${WEATHER_API_KEY}&dt=2010-01-01`
 
     axios
-    .get(currentWeatherFetch)
-    .then((response) => {
-      showSuccessSnack("Succesfull")
-      const weatherResponse = response.data;
-      setCurrentWeather({ city: searchData.label, ...weatherResponse });
-    })
-    .catch((err) => {
-      showErrorSnack("API is Unavailable")
-    });
+      .get(currentWeatherFetch)
+      .then((response) => {
+        showSuccessSnack("Succesfull");
+        const weatherResponse = response.data;
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+      })
+      .catch((err) => {
+        showErrorSnack("API is Unavailable");
+      });
 
     // axios
     // .get(historyFetch)
@@ -139,29 +133,31 @@ function App() {
     // });
 
     axios
-    .get(forecastFetch)
-    .then((response) => {
-      showSuccessSnack("Succesfull")
-      const forcastResponse = response.data.forecast.forecastday;
-      setForecast({ city: searchData.label, ...forcastResponse });
-    })
-    .catch((err) => {
-      showErrorSnack("API is Unavailable")
-    });
+      .get(forecastFetch)
+      .then((response) => {
+        showSuccessSnack("Succesfull");
+        const forcastResponse = response.data.forecast.forecastday;
+        setForecast({ city: searchData.label, ...forcastResponse });
+      })
+      .catch((err) => {
+        showErrorSnack("API is Unavailable");
+      });
   };
 
   return (
     <>
-      {preLoader ? <Loader /> : 
-      <div className="container">
-        <CF setCF = {setCF}/>
-        <Search onSearchChange={handleOnSearchChange} />
-        <Map handleOnLocationChange={handleOnLocationChange} />
-        {currentWeather && <CurrentWeather data={currentWeather} cf={cf}/>}
-        {/* {historyWeather && <HistoryWeather data={historyWeather} cf={cf}/>} */}
-        {forecast && <Forecast data={forecast} cf={cf}/>}
-      </div>
-      }
+      {preLoader ? (
+        <Loader />
+      ) : (
+        <div className="container">
+          <CF setCF={setCF} />
+          <Search onSearchChange={handleOnSearchChange} />
+          <Map handleOnLocationChange={handleOnLocationChange} />
+          {currentWeather && <CurrentWeather data={currentWeather} cf={cf} />}
+          {/* {historyWeather && <HistoryWeather data={historyWeather} cf={cf}/>} */}
+          {forecast && <Forecast data={forecast} cf={cf} />}
+        </div>
+      )}
     </>
   );
 }
--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,41 +1,52 @@
-import React from 'react'
+import React from "react";
 import "./Loader.css";
 
 function Loader() {
   return (
     <div className="loader">
-        <div className="about">
-            <a className="bg_links social portfolio" href="https://www.instagram.com/anirudhvadera/" target="_blank">
-                <span className="icon"></span>
-            </a>
-            <a className="bg_links social dribbble" href="https://github.com/ANIRUDH-VADERA" target="_blank">
-                <span className="icon"></span>
-            </a>
-            <a className="bg_links social linkedin" href="https://www.linkedin.com/in/anirudh-vadera-8836ba237/" target="_blank">
-                <span className="icon"></span>
-            </a>
-            <a className="bg_links logo"></a>
-        </div>
+      <div className="about">
+        <a
+          className="bg_links social portfolio"
+          href="https://www.instagram.com/omdhapodkar/"
+          target="_blank"
+        >
+          <span className="icon"></span>
+        </a>
+        <a
+          className="bg_links social dribbble"
+          href="https://github.com/OmDhapodkar"
+          target="_blank"
+        >
+          <span className="icon"></span>
+        </a>
+        <a
+          className="bg_links social linkedin"
+          href="https://www.linkedin.com/in/om-dhapodkar-485826205/"
+          target="_blank"
+        >
+          <span className="icon"></span>
+        </a>
+        <a className="bg_links logo"></a>
+      </div>
 
-        <div className="content">
-            <div className="planet">
-                <div className="ring"></div>
-                    <div className="cover-ring"></div>
-                <div className="spots">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-
-                </div>
-            </div>
-            <p>Loading...</p>
+      <div className="content">
+        <div className="planet">
+          <div className="ring"></div>
+          <div className="cover-ring"></div>
+          <div className="spots">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
         </div>
+        <p>Loading...</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Loader
+export default Loader;
